0bcacc95d5d0a72ef9bf4e253ac95c4c6aa6cc24
KrimyTarpara <krimytarpara@gmail.com>
2024-02-23 16:31:26 -0800

    Midterms

---
+++ src/midtermexam_versiona_extensioncode/krimy_Design.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Home
+ */
+public class krimy_Design {
+    // my favourite subject is Python 
+}
